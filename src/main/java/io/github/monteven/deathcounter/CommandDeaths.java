530dae02a51b37e7a394ee04aa1f181f9564d8f6
Monteven <montaguemoran01@gmaill.com>
2017-02-08 17:25:27 +0000

    Signed-off-by: Monteven <montaguemoran01@gmaill.com>

---
+++ src/main/java/io/github/monteven/deathcounter/CommandDeaths.java
@@ -0,0 +1,57 @@
+package io.github.monteven.deathcounter;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class CommandDeaths implements CommandExecutor {
+	private final DeathCounter plugin;
+	
+	public CommandDeaths(DeathCounter plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("deaths") && sender instanceof Player && sender.hasPermission("deathcounter.deaths")) {
+			
+			if(args.length < 1) {
+				Player player = (Player) sender;
+				String pUUID = player.getUniqueId().toString();
+				String pkUUID = player.getUniqueId().toString();
+				int pdeaths = plugin.getConfig().getInt("Players." + pUUID + ".Deaths");
+				int pkdeaths = plugin.getConfig().getInt("Players." + pkUUID + ".DeathsByPlayer");
+				player.sendMessage(color("&3You&7 have a total of &3" + pdeaths + " deaths&7,&3 " + pkdeaths + " &7of which are from &cenemy players&7."));
+				return true;
+			
+			}else if(args.length == 1) {
+				Player player = (Bukkit.getServer().getPlayer(args[0]));
+				if(player == null) {
+					sender.sendMessage(color("&3" + args[0] + " &7is not online!"));
+					return true;
+				
+				} else {
+					String pUUID = player.getUniqueId().toString();
+					String pkUUID = player.getUniqueId().toString();
+					int pdeaths = plugin.getConfig().getInt("Players." + pUUID + ".Deaths");
+					int pkdeaths = plugin.getConfig().getInt("Players." + pkUUID + ".DeathsByPlayer");
+					sender.sendMessage(color("&7The player&3 " + player.getName() + " &7has a total of&3 " + pdeaths + " deaths&7,&3 " + pkdeaths + " &7of which are from &cenemy players&7."));
+					return true;
+				}
+			}
+		} else if(cmd.getName().equalsIgnoreCase("deathshelp") && sender instanceof Player && sender.hasPermission("deathcounter.help")) {
+			Player player = (Player) sender;
+			player.sendMessage(color("&3Commands:\n/deaths &7- provides you with your total amount of deaths\n&3/deathshelp &7- provides you with help about Death Counter"));
+			return true;
+		}
+		return false;
+	}
+	
+	public String color(String msg) {
+		return ChatColor.translateAlternateColorCodes('&', msg);
+	}
+}
---
+++ src/main/java/io/github/monteven/deathcounter/DeathCounter.java
@@ -0,0 +1,78 @@
+package io.github.monteven.deathcounter;
+
+import java.io.File;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public final class DeathCounter extends JavaPlugin implements Listener {
+	
+	public static DeathCounter plugin;
+	
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+		getCommand("deaths").setExecutor(new CommandDeaths(this));
+		getCommand("deathshelp").setExecutor(new CommandDeaths(this));
+		getLogger().info("DeathCounter has been enabled.");
+		createConfig();
+	}
+	
+	private void createConfig() {
+	    try {
+	        if(!getDataFolder().exists()) {
+	            getDataFolder().mkdirs();
+	        }
+	        File file = new File(getDataFolder(), "config.yml");
+	        if(!file.exists()) {
+	            getLogger().info("Config.yml not found, creating!");
+	            saveDefaultConfig();
+	        }else{
+	            getLogger().info("Config.yml found, loading!");
+	        }
+	    } catch(Exception e) {
+	        e.printStackTrace();
+
+	    }
+
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		String uuid = player.getUniqueId().toString();
+		if(!getConfig().contains("Players." + uuid)) {
+			getConfig().set("Players." + uuid + ".Deaths", 0);
+			getConfig().set("Players." + uuid + ".DeathsByPlayer", 0);
+			saveConfig();
+		}
+	}
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+		String uuid = player.getUniqueId().toString();
+		int deaths = getConfig().getInt("Players." + uuid + ".Deaths");
+		if(player.getKiller() instanceof Player) {
+			int pkdeaths = getConfig().getInt("Players." + uuid + ".DeathsByPlayer");
+			getConfig().set("Players." + uuid + ".Deaths", deaths +1);
+			getConfig().set("Players." + uuid + ".DeathsByPlayer", pkdeaths +1);
+			saveConfig();
+			player.sendMessage(color("&cYou have died! Your total death count is now " + deaths+1 + "."));
+		}else{
+			getConfig().set("Players." + uuid + ".Deaths", deaths +1);
+			saveConfig();
+			player.sendMessage(color("&cYou have died! Your total death count is now " + deaths+1 + "."));
+		}
+	}
+	
+	public String color(String msg) {
+		return ChatColor.translateAlternateColorCodes('&', msg);
+	}
+}
